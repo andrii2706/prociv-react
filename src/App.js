601361934f802d10f649eef21c3d4ec34db241f5
GrayHead <ss.zhuravlov@gmail.com>
2021-06-24 12:49:22 +0300

    clan master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Product from './components/product/Product';
 
 function App() {
 
 	return (
 		<div className="App">
-			<Product title={'milk'} description={'asdadasda'}/>
-			<Product title={'meat'} description={'qweqweqwe'}/>
 
 
 		</div>
